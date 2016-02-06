1b6539e995142ef6f7afd77af47fb4bbf9c26e56
Matt Bierner <mattbierner@gmail.com>
2016-02-05 19:47:51 -0800

    inital commit

--- README.md
+++ README.md
@@ -1,31 +1,17 @@
-# I U+2764 Everyone
-To celebrate Valentine's Day, I've decided to use Twitter to send a personal and heartfelt message of love to each and every one of my 300 million fellow creatures. But, as much as I love all of you, the prospect of actually typing out 300 million tweets was just not that appealing, so instead I told my computer how to share love on my behalf.
+# I U+2764 Everything
+Fork of [I u2764 everyone](https://github.com/mattbierner/i-u2764-everyone) that sends messages of love to hashtags instead of people using Twitter.
 
-This repo contains a simple Twitter bot script that builds up a database of sad, lonely, little users and, once every five minutes, picks a lucky lover for whom the bot tweets, "@USER, I ❤️ you". Each user will be loved at most once. At this rate, the bot should finish its first iteration in around 3000 years.
+After @_LovedArt was banned for sending Twitter users love directly, I still lots of love to share. The ban made it clear that I should love things instead of people, which is why I created this bot and [@_lovedHArt][_lovedhart] to continue my work.
 
-[See it in action @\_LovedArt while you still can][_lovedart].
-
-### Why?
-Why not? The world needs more love.
-
-### Isn't this spam?
-No, the bot is designed to only contact users once. It also runs at a very low rate so as to not consume many of Twitter's resources. Nothing is being sold and there are [no malicious intentions](https://en.wikipedia.org/wiki/ILOVEYOU). All this bot does is post, "I ❤️ you", slowly working its way through the entire community of active Twitter users. That's it. Seriously.
-
-I feel that this bot uses Twitter as it was intended, and improves the Twitter community by making it a more fun and random place.
-
-### Will Twitter consider this spam?
-Probably. A fun little bot like this will probably get banned after a few hours, while advertisers are free to spew all sorts of shit at users. So it goes.
-
-Anyone is free to continue the mission of [@\_LovedArt][_lovedart] if it falls by banhammer. Love conquers all.
-
+[See the bot in action over @_lovedHArt][_lovedhart]
 
 # Running
-To spread the love yourself, first [register an application with Twitter](http://dev.twitter.com).
+To love yourself some hashtags, first [register an application with Twitter](http://dev.twitter.com).
 
 The main script for the bot lives in `index.js` and uses [Node][node].
 
 ```bash
-$ cd i_u2764_everyone
+$ cd i_u2764_everything
 $ npm install
 ```
 
@@ -38,11 +24,12 @@ $ export TWITTER_ACCESS_TOKEN_KEY="your access token key"
 $ export TWITTER_ACCESS_TOKEN_SECRET="your access token secret"
 ```
 
-Then just run the script, `$ node index.js`. A database of users that tracks who has been loved so far is saved to `data.db.
+Then just run the script, `$ node index.js`. A database of tags that tracks what has been loved so far is saved to `data.db.
 
-Run the script using [forever][forever], `$ forever start index.js`, so the love never has to stop.
+Run the script using [forever][forever], `$ forever start index.js`, so the #love never has to stop.
 
 
 [forever]: https://github.com/foreverjs/forever
 [node]: https://nodejs.org/
 [_lovedart]: https://twitter.com/_lovedart
+[_lovedhart]: https://twitter.com/_lovedhart
--- index.js
+++ index.js
@@ -11,14 +11,27 @@ const INTERVAL = 1000 * 60 * 5; // five minutes.
 const DB_FILE = "data.db";
 const DB_PATH = path.join(__dirname, DB_FILE);
 
-// Minimum number of unprocessed users to keep around at any given time.
+// Minimum number of unprocessed tags to keep around at any given time.
 const QUEUE_SIZE = 100;
 
-// Maximum number of users to sample each time. May not be unique.
+// Maximum number of tags to sample each time. May not be unique.
 const BUFFER_SIZE = 50;
 
-// Disable actual @mentions
-const DEBUG = false;
+// Disable actual postings
+const DEBUG = true;
+
+// Keep around in-memory cache to prevent getting stuck on a single item.
+const CACHE_LIMIT = 10;
+
+let temp_cache = [];
+
+const add_cache = (x) => {
+    temp_cache.unshift(x);
+    // unduplicate
+    temp_cache = temp_cache.filter((x, pos) => temp_cache.indexOf(x) === pos);
+    while (temp_cache.length > CACHE_LIMIT)
+        temp_cache.pop();
+};
 
 
 const client = new Twitter({
@@ -31,69 +44,62 @@ const client = new Twitter({
 const db = new Datastore({ filename: DB_FILE, autoload: true });
 
 /**
-    Select a user from the database,
+    Select a tag from the database,
 */
-const get_user_entry = (screenName, k) =>
-    db.findOne({ _id: screenName }, k);
+const get_tag_entry = (tag, k) =>
+    db.findOne({ tag: tag }, k);
 
 /**
-    Try to add a user to the database if one does not already exist.
+    Try to add a tag to the database if one does not already exist.
 */
-const ensure_user_entry = (screenName) =>
+const ensure_tag_entry = (tag) =>
     new Promise((resolve, reject) =>
-        get_user_entry(screenName, (err, result) => {
-            if (err)
-                return reject(err);
-            if (result !== null)
-                return resolve(result);
-            // Create new
-            db.insert({ _id: screenName, messaged_at: 0 },
-                (err, result) =>
-                    err ? reject(err) : resolve(result));
-        }));
+        db.update({ tag: tag }, { $set: { tag: tag } }, { upsert: true },
+            (err, result) =>
+                err ? reject(err) : resolve(result)));
 
 /**
-    Get users who are in database but have not been messaged yet.
+    Get tags who are in database but have not been messaged yet.
 */
-const get_unloved_user_entries = () =>
+const get_unloved_tag_entries = () =>
     new Promise((resolve, reject) =>
-        db.find({ messaged_at: 0 },
+        db.find({ messaged_at: { $exists: false } },
             (err, result) => err ? reject(err) : resolve(result)));
 
 /**
-    Start filling in the user database using the sample stream.
+    Start filling in the tag database using the sample stream.
 */
-const populate_users = (k) =>
+const populate_tags = (k) =>
     client.stream('statuses/sample', {}, (stream) => {
         stream.on('data', tweet => {
-            if (tweet && tweet.user && tweet.user.screen_name) {
-                const screenName = tweet.user.screen_name;
-                if (!k(screenName))
-                    stream.destroy();
+            if (tweet && tweet.entities && tweet.entities.hashtags) {
+                for (let tag of tweet.entities.hashtags)
+                    if (!k(tag.text))
+                        stream.destroy();
             }
         });
 
         stream.on('error', e => {
-            console.error(e);
+            console.error('stream error', e);
         });
     });
 
 /**
-    Try to make sure we have a good set of unloved users to pick from.
+    Try to make sure we have a good set of unloved tags to pick from.
 */
-const ensure_future_lovers = () =>
-    get_unloved_user_entries()
-        .then(users => {
-            if (users.length > QUEUE_SIZE)
+const ensure_future_tags = () =>
+    get_unloved_tag_entries()
+        .then(tags => {
+            if (tags.length > QUEUE_SIZE)
                 return;
 
             return new Promise((resolve) => {
                 let required = BUFFER_SIZE;
-                populate_users(screenName => {
-                    ensure_user_entry(screenName).catch(console.error);
+                populate_tags(tag => {
+                    ensure_tag_entry(tag).catch(e => console.error('ensure error', e));
                     if (required-- < 0) {
                         resolve();
-                        return false;
+                        return false; /* done */
                     }
                     return true; /* continue */
                 });
@@ -101,59 +107,77 @@ const ensure_future_lovers = () =>
         });
 
 /**
-    Select a sad, lonely user who needs some love.
+    Select a tag who needs some love.
 */
-const pick_user = () =>
+const pick_tag = () =>
     new Promise((resolve, reject) =>
-        db.findOne({ messaged_at: 0 },
+        db.findOne({ $and: [ { messaged_at: { $exists: false } }, { tag: { $nin: temp_cache } }] },
             (err, result) => {
                 if (err)
                     return reject(err);
                 if (!result)
-                    return reject("nobody to share the love with :(");
-                resolve(result._id);
+                    return reject("nothing to share the love with :(");
+                resolve(result.tag);
             }));
 
 /**
-    Make sure not to share too much love by marking when a user has been messaged.
+    Make sure not to share too much love by marking when a tag has been messaged.
 */
-const update_user = (screenName) =>
+const update_tag = (tag) =>
     new Promise((resolve, reject) =>
-        db.update({ _id: screenName },
+        db.update({ tag: tag },
             { $set: { messaged_at: Date.now() }},
             {},
-            (err) => err ? reject(err) : resolve(screenName)));
+            (err) => err ? reject(err) : resolve(tag)));
 
 /**
-    Generate a personalized messsage of heartfelt love.
+    Generate an expression of boundless love.
 */
-const status_message = (user) =>
-    (DEBUG ? user : '@' + user) + ', I \u{2764} you';
+const status_message = (tag) =>
+    'I \u{2764} #' + tag;
 
 /**
     Post the love.
 */
-const share_the_love = (screenName) =>
-    new Promise((resolve, reject) =>
-        client.post('statuses/update', { status: status_message(screenName) },
+const share_the_love = (tag) => {
+    add_cache(tag);
+
+    return new Promise((resolve, reject) =>
+        DEBUG ? resolve(tag) :
+        client.post('statuses/update', { status: status_message(tag) },
             (err, tweet, response) =>
-                err ? reject(err) : resolve(screenName)));
+                err ? reject(err) : resolve(tag)));
+};
 
 /**
     Spread love to the world.
 */
 const spread_the_love = () =>
-    ensure_future_lovers().then(() =>
-        pick_user()
-            .then(share_the_love)
-            .then(update_user)
-            .then(screenName => {
-                console.log('Shared the love with ' + screenName);
+    ensure_future_tags().then(pick_tag).then(tag =>
+        share_the_love(tag)
+            .then(update_tag)
+            .then(tag => {
+                console.log('Shared the love with ' + tag);
                 setTimeout(spread_the_love, INTERVAL);
             })
-            .catch((err) => {
-                console.error(err);
-                setTimeout(spread_the_love, INTERVAL);
-            }));
+            .catch(err => {
+                console.error('post error', err);
+                if (err && err[0] && err[0].code === 187) {
+                    console.error('Duplicate detected by Twitter, added entry to cache and will retry', tag);
+                    setTimeout(spread_the_love, 250);
+                    return;
+                } else {
+                    setTimeout(spread_the_love, INTERVAL);
+                }
+            }))
+    .catch(err => {
+        console.error('pick error', err);
+    });
 
-spread_the_love();
+db.ensureIndex({ fieldName: 'tag' }, err => {
+    if (err) {
+        console.error('index error', err);
+        return;
+    }
+    spread_the_love();
+});
